7fdaf36414ccc385d48441c69ba11779d16af0a0
sehdevkumar <kumarsehdev600@gmail.com>
2024-11-23 18:16:13 +0530

    more comments added

--- app/(home)/waterTracking/index.tsx
+++ app/(home)/waterTracking/index.tsx
@@ -2,6 +2,7 @@ import { SafeAreaView, Text, View } from "react-native";
 import { Colors } from "@/constants/Colors";
 import { BarChart } from "react-native-gifted-charts";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import BarCharts from "@/components/BarCharts";
 const IndexPage = () => {
   const barData = [{ value: 1 }, { value: 2 }, { value: 3 }, { value: 4 }];
 
@@ -17,7 +18,8 @@ const IndexPage = () => {
           alignItems: "center",
         }}
       >
-        <BarChart rulesType="solid" yAxisTextStyle={{ color: Colors.dark.text }} frontColor={"#177AD5"} barWidth={22} data={barData} />;
+        {/* <BarChart rulesType="solid" yAxisTextStyle={{ color: Colors.dark.text }} frontColor={"#177AD5"} barWidth={22} data={barData} />; */}
+        <BarCharts />
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ components/BarCharts.tsx
@@ -0,0 +1,62 @@
+import { Colors } from "@/constants/Colors";
+import { View } from "react-native";
+import { BarChart } from "react-native-gifted-charts";
+
+export default function BarCharts() {
+    const barData = [
+        {
+          value: 230,
+          label: 'Jan',
+          frontColor: '#4ABFF4',
+          sideColor: '#23A7F3',
+          topColor: '#92e6f6',
+        },
+        {
+          value: 180,
+          label: 'Feb',
+          frontColor: '#79C3DB',
+          sideColor: '#68BCD7',
+          topColor: '#9FD4E5',
+        },
+        {
+          value: 195,
+          label: 'Mar',
+          frontColor: '#28B2B3',
+          sideColor: '#0FAAAB',
+          topColor: '#66C9C9',
+        },
+        {
+          value: 250,
+          label: 'Apr',
+          frontColor: '#4ADDBA',
+          sideColor: '#36D9B2',
+          topColor: '#7DE7CE',
+        },
+        {
+          value: 320,
+          label: 'May',
+          frontColor: '#91E3E3',
+          sideColor: '#85E0E0',
+          topColor: '#B0EAEB',
+        },
+    ];
+    return (
+        <View>
+            <BarChart
+            showFractionalValues
+            showYAxisIndices
+            hideRules
+            noOfSections={4}
+            maxValue={400}
+            data={barData}
+            barWidth={40}
+            sideWidth={15}
+            isThreeD 
+            side="right"
+            yAxisTextStyle={{ color: Colors.dark.text }}
+            xAxisColor={Colors.dark.text}
+            xAxisLabelTextStyle={{ color: Colors.dark.text }}
+            />
+        </View>
+    );
+}
